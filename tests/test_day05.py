9f01e2ae473d60561107063dd17add6d07393258
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2021-12-06 21:30:54 -0500

    Add solutions for day 05

---
+++ tests/test_day05.py
@@ -0,0 +1,11 @@
+from aoc2021 import day05
+
+
+def test_day05_part1():
+    data_path = "data/day05-sample.txt"
+    assert day05.part1(data_path) == 5
+
+
+def test_day05_part2():
+    data_path = "data/day05-sample.txt"
+    assert day05.part2(data_path) == 12
