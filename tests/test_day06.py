24a9aded874b6153582915d1005475186350175a
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2021-12-06 22:09:41 -0500

    Extend unit tests to include full inputs

--- tests/test_day06.py
+++ tests/test_day06.py
@@ -1,11 +1,21 @@
+import pytest
+
 from aoc2021 import day06
 
 
-def test_day06_part1():
-    data_path = "data/day06-sample.txt"
-    assert day06.part1(data_path) == 5934
+@pytest.mark.parametrize(
+    ("name", "expected"),
+    [("sample", 5934), ("full", 350917)],
+)
+def test_day06_part1(name: str, expected: int):
+    data_path = f"data/day06-{name}.txt"
+    assert day06.part1(data_path) == expected
 
 
-def test_day06_part2():
-    data_path = "data/day06-sample.txt"
-    assert day06.part2(data_path) == 26984457539
+@pytest.mark.parametrize(
+    ("name", "expected"),
+    [("sample", 26984457539), ("full", 1592918715629)],
+)
+def test_day06_part2(name: str, expected: int):
+    data_path = f"data/day06-{name}.txt"
+    assert day06.part2(data_path) == expected
